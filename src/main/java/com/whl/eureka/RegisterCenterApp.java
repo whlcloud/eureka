652c0e12b4b1d55a940c5625320dc9f6d0508ff0
WHaiLong <18419360859@163.com>
2019-06-16 21:17:10 +0800

    首次提交

---
+++ src/main/java/com/whl/eureka/RegisterCenterApp.java
@@ -0,0 +1,13 @@
+package com.whl.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterCenterApp {
+	public static void main(String[] args) {
+        SpringApplication.run(RegisterCenterApp.class, args);
+    }
+}
